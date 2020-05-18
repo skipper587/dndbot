cd95e4cdd68fd6db276d650b014770f6fdd85cc1
skipper587 <45574121+skipper587@users.noreply.github.com>
2020-05-17 18:23:26 -0700

    Beta 0.8

--- bot.py
+++ bot.py
@@ -14,10 +14,10 @@ import math
 ##--------------------Global Variables--------------------##
 
 DNDBot = discord.Client()  # Initialize Client
-dndbot = commands.Bot(command_prefix="?")  # Initialize client bot
+dndbot = commands.Bot(command_prefix="-")  # Initialize client bot
 dndbot.remove_command("help")
 
-versionnumber = "0.5"
+versionnumber = "Beta 0.8"
 
 ##--------------------Global Functions--------------------##
 
@@ -35,8 +35,8 @@ async def on_member_join(member):
         bot = dndbot.get_user(710630587366375446)
         message_channel = dndbot.get_channel(710611293484220498)
         
-        embed=discord.Embed(color=0xaa0000)
-        embed.add_field(name="//Send Greeting", value="Hello **" + str(member.name) + "**, and welcome to the Giant's Ring. I am the local AI. For more information on how I can help you, send me a DM with ?help.", inline=False)
+        embed=discord.Embed(color=0x00c600)
+        embed.add_field(name="//Send Greeting", value="Hello %s, and welcome to the Giant's Ring. I am the local AI. For more information on how I can help you, send me a DM with ?help." % member.mention, inline=False)
         
         await message_channel.send(embed=embed)
         
@@ -45,16 +45,29 @@ async def on_member_join(member):
 async def botquit(ctx):
         user = ctx.message.author
         if user.top_role.name == "DM":
-                await ctx.send("Shutting down.")
+                embed=discord.Embed(color=0x00c600)
+                embed.add_field(name="//Credentials Verified", value="Shutting down.", inline=True)
+
+                await ctx.send(embed=embed)
+                
                 await dndbot.close()
                 await DNDBot.close()
                 quit()
+        else:
+                embed=discord.Embed(color=0xaa0000)
+                embed.add_field(name="//Invalid Credentials", value="Failure, you lack the authorization to execute this subroutine!", inline=True)
+
+                await ctx.send(embed=embed)
 
 ## Version
-@dndbot.command(name='version')
+@dndbot.command(name='version',aliases=['v'])
 async def version(ctx):
         displayversion = ["Version: ", versionnumber]
-        await ctx.send(''.join(displayversion))
+
+        embed=discord.Embed(color=0x00c600)
+        embed.add_field(name="//Local Version", value=''.join(displayversion), inline=True)
+
+        await ctx.send(embed=embed)
 
 ## About
 @dndbot.command(name='about')
@@ -63,15 +76,14 @@ async def about(ctx):
         path = 'files/'
         numRecords = len([f for f in os.listdir(path)if os.path.isfile(os.path.join(path, f))])
         
-        embed=discord.Embed(color=0xaa0000)
-        embed.set_author(name=bot.name,icon_url=bot.avatar_url)
-        embed.add_field(name="//Send Greeting", value="Hello! I am the Archive AI, keeper of the history of the Giant's Ring.", inline=False)
+        embed=discord.Embed(color=0x00c600)
+        embed.add_field(name="//Send Greeting", value="Hello! I am the Archives AI, keeper of the history of the Giant's Ring.", inline=False)
         embed.add_field(name="//State Directive", value="I exist to help preserve the history of the Giant's Ring for those who seek it. As you search records, you will find that any __underlined__ keyword can be searched in the Archives, if you have the clearance. Simply query me with ?info `query`\n \n The Archives currently contain " + str(numRecords) + " accessible records.", inline=False)
 
         await ctx.send(embed=embed)
 
 ## Stats
-@dndbot.command(name="stats",aliases=['update'])
+@dndbot.command(name="stats",aliases=['update','s'])
 async def stats(ctx, *args):
         bot = dndbot.get_user(710630587366375446)
                 
@@ -94,7 +106,7 @@ async def stats(ctx, *args):
                 world = fr.read()
                 fr.close()
 
-                embed=discord.Embed(color=0xaa0000)
+                embed=discord.Embed(color=0x00c600)
                 embed.add_field(name="//Sessions", value=sessions, inline=True)
                 embed.add_field(name="//Combat", value=combat, inline=True)
                 embed.add_field(name="//World", value=world, inline=True)
@@ -102,7 +114,7 @@ async def stats(ctx, *args):
                 await ctx.send(embed=embed)
 
         if args:
-                if len(args) == 3:
+                if len(args) == 3 and ctx.message.author.top_role.name == "DM":
                         file = ''.join(args[0])
                         line = int(''.join(args[1]))
                         value = ''.join(args[2])
@@ -119,24 +131,26 @@ async def stats(ctx, *args):
                         fr.writelines(data)
 
                         embed=discord.Embed(color=0x00c600)
-                        embed.add_field(name="//Send Confirmation", value="**Success**", inline=True)
+                        embed.add_field(name="//Send Confirmation", value="Success, updated campaign stats.", inline=True)
 
                         await ctx.send(embed=embed)
 
                 elif len(args) < 3:
                         embed=discord.Embed(color=0xaa0000)
-                        embed.add_field(name="//Send Confirmation", value="**Failure, missing parameters!**", inline=True)
+                        embed.add_field(name="//Send Confirmation", value="Failure, missing parameters!", inline=True)
 
                         await ctx.send(embed=embed)
                 else:
                         embed=discord.Embed(color=0xaa0000)
-                        embed.add_field(name="//Send Confirmation", value="**Failure, unexpected parameters!**", inline=True)
+                        embed.add_field(name="//Send Confirmation", value="Failure, unexpected parameters!", inline=True)
 
                         await ctx.send(embed=embed)
 
 ## Info Command (Primary Functionality)
-@dndbot.command(name='info')
+@dndbot.command(name='info',aliases=['i'])
 async def info(ctx, query, *subrecord):
+        bot = dndbot.get_user(710630587366375446)
+    
         if subrecord:
                 subrecord = ''.join(subrecord)
                 query = query + " " + subrecord
@@ -159,7 +173,10 @@ async def info(ctx, query, *subrecord):
                 if ctx.author.dm_channel is None:
                         await ctx.author.create_dm()
 
-                await ctx.send("Retrieving known information about your query.")
+                embed=discord.Embed(color=0x00c600)
+                embed.add_field(name="//Retrieve Record", value="Success, retrieving known information about your query.", inline=True)
+
+                await ctx.send(embed=embed)
                 
                 if len(fileContents) > 1024:
                         numOut = len(fileContents) / 1024
@@ -174,14 +191,14 @@ async def info(ctx, query, *subrecord):
                                 index2 = index2 + 1024
                                 
                                 if not (i + 1) > 1:     
-                                        embed=discord.Embed(title=name, color=0xaa0000)
-                                        embed.set_author(name=ctx.author,icon_url=ctx.author.avatar_url)
+                                        embed=discord.Embed(title=name, color=0x00c600)
+                                        embed.set_author(name=bot.name,icon_url=bot.avatar_url)
                                         embed.add_field(name="Known Info:", value=newContents[i], inline=True)
                                         embed.set_footer(text="Last Updated: " + modified + " | Part 1 of " + str(math.ceil(numOut)))
                         
                                         await ctx.author.send(embed=embed)
                                 else:
-                                        embed=discord.Embed(color=0xaa0000)
+                                        embed=discord.Embed(color=0x00c600)
                                         embed.add_field(name="Continued:", value=newContents[i], inline=True)
                                         embed.set_footer(text="Last Updated: " + modified + " | Part " + str(i + 1) + " of " + str(math.ceil(numOut)))
                         
@@ -189,22 +206,25 @@ async def info(ctx, query, *subrecord):
 
                                 i += 1
                 else:
-                        embed=discord.Embed(title=name, color=0xaa0000)
-                        embed.set_author(name=ctx.author,icon_url=ctx.author.avatar_url)
+                        embed=discord.Embed(title=name, color=0x00c600)
+                        embed.set_author(name=bot.name,icon_url=bot.avatar_url)
                         embed.add_field(name="Known Info:", value=fileContents, inline=True)
                         embed.set_footer(text="Last Updated: " + modified)
 
                         await ctx.author.send(embed=embed)
                 
-                print("Sent " + ctx.author.name + " the contents of the " + query + ".txt file.")
+                print("Sent " + ctx.author.name + " the contents of the " + query + " record.")
         else:
-                await ctx.send("Your party has not discovered any information about that yet!")
+                embed=discord.Embed(color=0xaa0000)
+                embed.add_field(name="//Invalid Credentials", value="Failure, your party does not have access to that information yet!", inline=True)
+
+                await ctx.send(embed=embed)
 
 ## Help Command
-@dndbot.command(name='help')
+@dndbot.command(name='help',aliases=['h'])
 async def help(ctx, *command):
         if not command:
-                embed=discord.Embed(title="Available Commands", color=0xaa0000)
+                embed=discord.Embed(title="Available Commands", color=0x00c600)
                 embed.add_field(name="?info", value="Returns information on a specified record.", inline=False)
                 embed.add_field(name="?version", value="Shows the current version of the bot.", inline=False)
                 embed.set_footer(text="Use ?help and a command name for more information on a specific command.")
@@ -214,13 +234,16 @@ async def help(ctx, *command):
         if command:
                 commandName = ''.join(command)
                 if commandName == "info":
-                        embed=discord.Embed(title="?info", color=0xaa0000)
+                        embed=discord.Embed(title="?info - Basic query command, returns a record.", color=0x00c600)
                         embed.add_field(name="Syntax", value="?info `query` `subrecord`", inline=False)
                         embed.add_field(name="Notes", value="`query` must be \"surrounded by double quotations\" if it consists of multiple words.\n eg. ?info \"query name\"\n \n Subrecords only exist if specified in a main record file.", inline=False)
                         
                         await ctx.send(embed=embed)
                 else:
-                        await ctx.send("No help log exists for the specified command.")
+                        embed=discord.Embed(color=0xaa0000)
+                        embed.add_field(name="//Exception", value="Failure, no help log exists for that subroutine.", inline=True)
+
+                        await ctx.send(embed=embed)
 
 # Replace token with your bot's token
 dndbot.run("Private Key")
